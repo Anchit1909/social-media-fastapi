a4b4417068bf69da38fba56a6d2744c48ef2f44d
Anchit Sinha <anchit1909@gmail.com>
2024-07-01 00:05:31 +0530

    feat: basic app complete

---
+++ alembic/versions/11f378e38830_create_all_tables.py
@@ -0,0 +1,24 @@
+"""create all tables
+
+Revision ID: 11f378e38830
+Revises: 
+Create Date: 2024-06-30 12:43:29.340805
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '11f378e38830'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
